cc797332678ba803332828bb985759e711ff8ab1
Kornyshev <kornyshev.e@yandex.ru>
2020-08-02 22:10:47 +0300

    2.08.2020 - Homework is done (First edition)

---
+++ src/main/java/ForExp.java
@@ -0,0 +1,5 @@
+public class ForExp {
+    public static void main(String[] args) {
+        System.out.println(1000L / 51L);
+    }
+}
---
+++ src/test/java/hw1/tests/CalculatorSubTest.java
@@ -0,0 +1,23 @@
+package hw1.tests;
+
+import hw1.CommonConditions;
+import hw1.DataProviderForCalc;
+import org.testng.annotations.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+public class CalculatorSubTest extends CommonConditions {
+
+    @Test(dataProvider = "CorrectIntTestData", dataProviderClass = DataProviderForCalc.class)
+    public void testSubLongValues(long x, long y) {
+        long expected = x - y;
+        assertThat(calculator.sub(x, y), equalTo(expected));
+    }
+
+    @Test(dataProvider = "CorrectDoubleTestData", dataProviderClass = DataProviderForCalc.class)
+    public void testSubDoubleValues(double x, double y) {
+        double expected = x - y;
+        assertThat(calculator.sub(x, y), equalTo(expected));
+    }
+}
