ec662394c860a8ea3f7be769501eafc709eab012
Kornyshev <kornyshev.e@yandex.ru>
2020-08-18 14:25:38 +0300

    18.08.2020 - Changing driver from Headless to regular

--- src/test/java/hw4/driver/DriverFactory.java
+++ src/test/java/hw4/driver/DriverFactory.java
@@ -3,14 +3,12 @@ package hw4.driver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverFactory {
 
     public static WebDriver create() {
         WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver(
-                new ChromeOptions().addArguments("headless"));
+        WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         return driver;
     }
